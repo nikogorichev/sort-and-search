0998c9867a8c93df9955550e4cd6e32abe325806
Николай Горичев <nikogorichev812@gmail.com>
2025-07-05 10:56:45 +0300

    add bubble sort

---
+++ src/model/sorting/bubbleSort.ts
@@ -0,0 +1,22 @@
+export function bubbleSort<T>(
+  list: Array<T>,
+  key: keyof T,
+  ASC: boolean = true
+): Array<T> {
+  const result = [...list];
+  const n = result.length;
+
+  for (let i = 0; i < n - 1; i++) {
+    for (let j = 0; j < n - i - 1; j++) {
+      const a = result[j][key];
+      const b = result[j + 1][key];
+      const compare = a < b ? -1 : a > b ? 1 : 0;
+
+      const shouldSwap = ASC ? compare > 0 : compare < 0;
+      if (shouldSwap) {
+        [result[j], result[j + 1]] = [result[j + 1], result[j]];
+      }
+    }
+  }
+  return result;
+}
